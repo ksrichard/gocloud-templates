242bfac7cba9c3b399056d88bbc52652813e9c26
ksrichard <richard.klavora@hazelcast.com>
2021-02-15 12:12:29 +0100

    added local login option

--- infra/kubernetes/install.sh
+++ infra/kubernetes/install.sh
@@ -1,3 +1,9 @@
+{{#pulumi_local_login}}
+pulumi login --local
+{{/pulumi_local_login}}
+{{^pulumi_local_login}}
+pulumi login
+{{/pulumi_local_login}}
 pulumi stack init {{stack}}
 pulumi stack select {{stack}}
 npm install
