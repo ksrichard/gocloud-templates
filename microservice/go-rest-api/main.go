7118cae7011be96a49f3aa9b6b600a728c708ce6
ksrichard <richard.klavora@hazelcast.com>
2021-03-17 10:56:09 +0100

    added main.go

---
+++ microservice/go-rest-api/main.go
@@ -0,0 +1,25 @@
+package
+
+import "os"
+{{go_module_name.Value}}
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+var ROOT_PATH = os.Getenv("ROOT_PATH")
+var QUERY_PATH = os.Getenv("QUERY_PATH")
+var HEALTH_CHECK_PATH = os.Getenv("HEALTH_CHECK_PATH")
+
+func main() {
+	// web server
+	r := gin.Default()
+	root := r.Group(ROOT_PATH)
+	{
+		root.GET(HEALTH_CHECK_PATH, func(c *gin.Context) {
+			c.Status(200)
+		})
+	}
+	r.Run()
+}
